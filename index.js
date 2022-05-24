2d4a98bb387941e7c4a1e9a3a4c8e4e179162698
bhaskarpraveen <praveennaidu264@gmail.com>
2022-05-24 10:18:58 +0530

    added cors

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 //import routes
 const userRoutes = require('./routes/user_routes');
 
-
+const corsOpts={
+    origin:["http://localhost:4200"],
+    methods:[
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE'
+    ],
+    allowHeaders:[
+        '*'
+    ]
+}
 //middlewares
 app.use(express.json())
+app.use(cors(corsOpts))
 
 
 
